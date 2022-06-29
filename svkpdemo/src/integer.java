3efe968934cbf9f8a0b93b828307315ca7105e00
acer <acer@TMP249-G2-M.bbrouter>
2022-06-29 10:57:00 +0530

    unicode

--- svkpdemo/src/integer.java
+++ svkpdemo/src/integer.java
@@ -1,4 +1,4 @@
-public class Hello {
+public class integer {
 	public static void main(String[] args) {
 		int max = 2147483647;
 		int min = -2147483648;
@@ -8,5 +8,5 @@ public class Hello {
 		long longMin = -9223372036854775808L;
 		byte byteMax = 127;
 		byte byteMin = -128;
+		}
 	}
-}
