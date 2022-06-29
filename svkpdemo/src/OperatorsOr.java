5d721193c7f9e6075559e84f17ac4497f334b990
acer <acer@TMP249-G2-M.bbrouter>
2022-06-29 11:35:54 +0530

    OperatorsOr

---
+++ svkpdemo/src/OperatorsOr.java
@@ -0,0 +1,25 @@
+public class OperatorsOr {
+	public static void main(String[] args) {
+		int x = 10; int y = 11;
+		if(!(x < y) || (x == y))
+		{
+			System.out.println("Condition is TRUE");
+			}
+		else
+		{
+			System.out.println("Condition is FALSE");
+			}
+/*
+int ageOfBoy = 45;
+int ageOfGirl = 32;
+if ((ageOfBoy &gt;= 21) &amp;&amp; (ageOfGirl &gt;= 18))
+{
+System.out.println(&quot;ready to get married!&quot;);
+}
+else
+{
+System.out.println(&quot;Wait for it kiddo!&quot;);
+}
+*/
+}
+}
