2616820f6898db88b16925354b0cef6facd731cb
acer <acer@TMP249-G2-M.bbrouter>
2022-06-29 11:12:55 +0530

    arithmetic

---
+++ svkpdemo/src/arithmetic.java
@@ -0,0 +1,8 @@
+public class arithmetic {
+	public static void main(String[] args) {
+		int x = 10, y=20;
+		System.out.println(x+y);
+		System.out.println(--x);
+		System.out.println(++y);
+		}
+	}
