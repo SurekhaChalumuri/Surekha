16885e33db61d9e378981de0fd27675be5871295
acer <acer@TMP249-G2-M.bbrouter>
2022-06-29 11:19:40 +0530

    operators

---
+++ svkpdemo/src/operators.java
@@ -0,0 +1,14 @@
+public class operators {
+	public static void main(String[] args)
+	{
+		int x = 3;
+		if(x != 3)
+		{
+			System.out.println("Value of x is not 3");
+			}
+		else
+		{
+			System.out.println("Value of x is 3");
+			}
+		}
+	}
