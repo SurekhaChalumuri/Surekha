b73aa71d5b7491e6c2ec51eadd60be9d5dfadcdc
acer <acer@TMP249-G2-M.bbrouter>
2022-06-29 11:24:44 +0530

    OperatorsAnd

---
+++ svkpdemo/src/OperatorsAnd.java
@@ -0,0 +1,14 @@
+public class OperatorsAnd {
+	public static void main(String[] args)
+	{
+		int x = 6, y = 3;
+		if(x >= y)
+		{
+			System.out.println("true");
+			}
+		else
+		{
+			System.out.println(false);
+			}
+		}
+	}
