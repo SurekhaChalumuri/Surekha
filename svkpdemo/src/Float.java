93d73a04754938ef9095474ecb95db2c4fe74fd4
acer <acer@TMP249-G2-M.bbrouter>
2022-06-29 10:45:09 +0530

    data types

---
+++ svkpdemo/src/Float.java
@@ -0,0 +1,15 @@
+public class Float {
+	public static void main(String[] args) {
+		/*
+		  int value1 = 7/2;
+		  float value2 = 8f/4f;
+		  double value3 = 10d/6d;
+		  System.out.println("value 1 = "+ value1);
+		  System.out.println("value 2 = "+ value2);
+		  System.out.println("value 3 = "+ value3);
+		  */
+		int marker = 256;
+		double percentage = marker * 0.46f;
+		System.out.println("percentage:"+percentage);
+		}
+	}
